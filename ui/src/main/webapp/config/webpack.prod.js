1f07a8e48b81590bb444d075d5b725cc6378f863
David Klingenberg <dklingen@redhat.com>
2016-10-21 14:41:20 +0200

    Change path for production env.

--- ui/src/main/webapp/config/webpack.prod.js
+++ ui/src/main/webapp/config/webpack.prod.js
@@ -12,17 +12,17 @@ module.exports = webpackMerge(commonConfig, {
     output: {
         path: helpers.root('dist'),
         publicPath: '/',
-        filename: '[name].[hash].js',
-        chunkFilename: '[id].[hash].chunk.js'
+        filename: 'js/[name].[hash].js',
+        chunkFilename: 'js/[id].[hash].chunk.js'
     },
-
+/*
     htmlLoader: {
         minimize: false // workaround for ng2
     },
-
+*/
     plugins: [
         new webpack.NoErrorsPlugin(),
-        new webpack.optimize.DedupePlugin(),
+//        new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
             mangle: {
                 keep_fnames: true
