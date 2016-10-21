112fbf9a6b0532363c44dbc4cb91cbce2cd79301
David Klingenberg <dklingen@redhat.com>
2016-10-21 14:39:09 +0200

    Webpack config - better way to identify environment.

--- ui/src/main/webapp/webpack.config.js
+++ ui/src/main/webapp/webpack.config.js
@@ -1,15 +1,25 @@
+module.exports = function(environment) {
+  var mode;
 
-switch (process.env.NODE_ENV) {
-  case 'prod':
-  case 'production':
-    module.exports = require('./config/webpack.prod');
+  if (environment.hasOwnProperty('environment')) {
+    mode = environment.environment;
+  } else {
+    mode = process.env.NODE_ENV;
+  }
+
+  switch (mode) {
+    case 'prod':
+    case 'production':
+      return require('./config/webpack.prod');
+      break;
+    case 'test':
+    case 'testing':
+      return  require('./config/webpack.test');
+      break;
+    case 'dev':
+    case 'development':
+    default:
+      return require('./config/webpack.dev');
     break;
-  case 'test':
-  case 'testing':
-    module.exports = require('./config/webpack.test');
-    break;
-  case 'dev':
-  case 'development':
-  default:
-    module.exports = require('./config/webpack.dev');
-}
+  }
+};
