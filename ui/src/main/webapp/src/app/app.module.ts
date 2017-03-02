19e0e7ae5f5976cb36090f1cb6df432fe820d010
David Klingenberg <dklingen@redhat.com>
2017-03-02 10:37:25 +0100

    Remove executions things from app module

--- ui/src/main/webapp/src/app/app.module.ts
+++ ui/src/main/webapp/src/app/app.module.ts
@@ -14,16 +14,11 @@ import {WindupService} from "./services/windup.service";
 import {EditApplicationFormComponent} from "./registered-application/edit-application-form.component";
 import {FramesRestClientService} from './services/graph/frames-rest-client.service';
 import {ApplicationResolve} from "./registered-application/application.resolve";
-import {ExecutionsListComponent} from "./executions/executions-list.component";
-import {AllExecutionsComponent} from "./executions/all-executions.component";
-import {GroupExecutionsComponent} from "./executions/group-executions.component";
 import {FileModelService} from "./services/graph/file-model.service";
 import {ClassificationService} from "./services/graph/classification.service";
 import {HintService} from "./services/graph/hint.service";
 import {WindupExecutionService} from "./services/windup-execution.service";
-import {ActiveExecutionsProgressbarComponent} from "./executions/active-executions-progressbar.component";
 import {initializeModelMappingData} from "./generated/tsModels/discriminator-mapping-data";
-import {ExecutionDetailComponent} from "./executions/execution-detail.component";
 import {GraphJSONToModelService} from "./services/graph/graph-json-to-model.service";
 import {LoginComponent} from "./components/login.component";
 import {SharedModule} from "./shared/shared.module";
@@ -33,6 +28,7 @@ import {AnalysisContextModule} from "./analysis-context/analysis-context.module"
 import {ConfigurationModule} from "./configuration/configuration.module";
 import {GroupModule} from "./group/group.module";
 import {ProjectModule} from "./project/project.module";
+import {ExecutionsModule} from "./executions/executions.module";
 
 /**
  * Load all mapping data from the generated files.
@@ -51,20 +47,15 @@ initializeModelMappingData();
         AnalysisContextModule,
         ConfigurationModule,
         GroupModule,
-        ProjectModule
+        ProjectModule,
+        ExecutionsModule
     ],
     declarations: [
         // pages
         AppComponent,
         LoginComponent,
         RegisterApplicationFormComponent,
-        EditApplicationFormComponent,
-
-        ExecutionsListComponent,
-        AllExecutionsComponent,
-        GroupExecutionsComponent,
-        ActiveExecutionsProgressbarComponent,
-        ExecutionDetailComponent
+        EditApplicationFormComponent
     ],
     providers: [
         appRoutingProviders,
