9004c5f25a7fc79e1931475a9ba52971cce0c580
David Klingenberg <dklingen@redhat.com>
2017-03-22 16:03:57 +0100

    [WIP] - application level routes

--- ui/src/main/webapp/src/app/reports/application-level-layout.component.ts
+++ ui/src/main/webapp/src/app/reports/application-level-layout.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {ExecutionsLayoutComponent} from "../executions/executions-layout.component";
 import {WindupExecution, RegisteredApplication} from "windup-services";
 import {RouteLinkProviderService} from "../core/routing/route-link-provider-service";
@@ -7,35 +7,57 @@ import {EventBusService} from "../core/events/event-bus.service";
 import {MigrationProjectService} from "../project/migration-project.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {DatePipe} from "@angular/common";
+import {RouteFlattenerService} from "../core/routing/route-flattener.service";
+import {FilterApplication} from "windup-services";
+
+type Application = any; //RegisteredApplication|FilterApplication;
 
 @Component({
     templateUrl: './application-level-layout.component.html'
 })
-export class ApplicationLevelLayoutComponent extends ExecutionsLayoutComponent {
-    public allApplications: RegisteredApplication[];
-    public application: RegisteredApplication;
+export class ApplicationLevelLayoutComponent extends ExecutionsLayoutComponent implements OnInit {
+    public allApplications: (RegisteredApplication|FilterApplication)[];
+    public application: RegisteredApplication|FilterApplication;
 
     constructor(
+        _router: Router,
         _activatedRoute: ActivatedRoute,
+        _routeFlattenerService: RouteFlattenerService,
         _routeLinkProviderService: RouteLinkProviderService,
         _migrationProjectService: MigrationProjectService,
         _eventBus: EventBusService,
-        _router: Router,
         _windupService: WindupService,
         _datePipe: DatePipe
     ) {
-        super(_activatedRoute, _routeLinkProviderService, _migrationProjectService, _eventBus, _router, _windupService, _datePipe);
+        super(
+            _router,
+            _activatedRoute,
+            _routeFlattenerService,
+            _routeLinkProviderService,
+            _migrationProjectService,
+            _eventBus,
+            _windupService,
+            _datePipe
+        );
     }
 
-    public getApplicationLabel = (execution: WindupExecution): string => {
-        return execution ? this._datePipe.transform(execution.timeStarted, 'short') : '';
+
+    ngOnInit(): void {
+        super.ngOnInit();
+        this.flatRouteLoaded.subscribe(data => {
+            this.allApplications = this.execution.filterApplications;
+        });
+    }
+
+    public getApplicationLabel = (application: Application): string => {
+        return application.fileName;
     };
 
-    public getApplicationRoute = (execution: WindupExecution): any[] => {
-        return execution ? ['/projects', this.project.id, 'reports', execution.id] : null;
+    public getApplicationRoute = (application: Application): any[] => {
+        return application ? ['/projects', this.project.id, 'reports', this.execution.id, 'applications', application.id] : null;
     };
 
-    public navigateToApplication = (execution: WindupExecution) => {
-        this._router.navigate(this.getExecutionRoute(execution));
+    public navigateToApplication = (application: Application) => {
+        this._router.navigate(this.getApplicationRoute(application));
     };
 }
